237ac568d71560165ac9cc8d1925c6404b76f96f
DevinDon <i.inf@outlook.com>
2021-07-09 19:20:45 +0800

    perf: perf demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -4,7 +4,7 @@ import { Category, fetchArticle, fetchComments } from '../main';
 (async () => {
   const id = 3795;
   const article = await fetchArticle(id);
-  logger.debug(`fetched article ${id}, content:\n`, article);
+  logger.debug(`fetched article ${id}, content:\n`, JSON.stringify(article));
   const comments = await fetchComments(Category.Article, id);
   logger.debug(`fetched comments of article ${id}, content:\n`, comments.length);
 })();
